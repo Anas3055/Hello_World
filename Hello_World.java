13048c291c101f488cfdbad043c5239ac118f973
Anas3055 <115462732+Anas3055@users.noreply.github.com>
2022-10-27 14:23:28 +0200

    Update Hello_World.java

--- Hello_World.java
+++ Hello_World.java
@@ -1,9 +1,7 @@
 
 public class Hello_World {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+	public static void main(String[] args) {	
 		
 		System.out.println("Hello World");
 	}
